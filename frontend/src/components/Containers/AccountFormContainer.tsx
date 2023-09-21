8cca2b206b002687be03bd550be9c2e035550b8f
aaronmg241 <agrubb@uoguelph.ca>
2023-09-20 23:11:07 -0300

    Added responsive styling to login pages.

--- frontend/src/components/Containers/AccountFormContainer.tsx
+++ frontend/src/components/Containers/AccountFormContainer.tsx
@@ -1,14 +1,32 @@
 import { Paper, createStyles, Flex, Text } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 import { IconWind } from '@tabler/icons-react'
 
 const useStyles = createStyles((theme) => ({
 	form: {
 		borderRadius: '0.5rem',
-		width: 'min(50vw, 500px)',
+		width: 'min(85vw, 500px)',
 		background: theme.colors.dark[8],
-
-		[theme.fn.smallerThan('sm')]: {
-			maxWidth: '100%',
+		padding: '4rem',
+		[theme.fn.smallerThan('md')]: {
+			padding: '2rem',
+		},
+	},
+	title: {
+		fontWeight: 'bold',
+		color: 'white',
+		fontSize: 40,
+		[theme.fn.smallerThan('md')]: {
+			fontSize: 32,
+		},
+	},
+	icon: {
+		color: '#4F7CAC',
+		height: 60,
+		width: 60,
+		[theme.fn.smallerThan('md')]: {
+			height: 45,
+			width: 45,
 		},
 	},
 }))
@@ -18,12 +36,10 @@ export default function AccountFormContainer({ children }: { children: React.Rea
 
 	return (
 		<Flex w='100%' h='100%' align='center' justify='center'>
-			<Paper className={classes.form} radius={0} p='4rem 4rem' style={{ overflow: 'hidden' }}>
+			<Paper className={classes.form} p='4rem' style={{ overflow: 'hidden' }}>
 				<Flex gap='10px' mb={30} style={{ alignSelf: 'center' }} justify='center' align='center'>
-					<IconWind style={{ color: '#4F7CAC', height: 60, width: 60 }} />
-					<Text fw='bold' color='white' fz='40px'>
-						FullClear
-					</Text>
+					<IconWind className={classes.icon} />
+					<Text className={classes.title}>FullClear</Text>
 				</Flex>
 				{children}
 			</Paper>
